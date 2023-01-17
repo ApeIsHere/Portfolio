fcf2bc99d9125192895896e16da7cea13f4489d6
ApeIsHere <53047099+ApeIsHere@users.noreply.github.com>
2023-01-17 15:48:20 +0300

    Skills Done + script

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,9 @@ close.addEventListener('click', () => {
 });
 
 
+const counters = document.querySelectorAll('.skills__progress__item-percent'),
+      lines = document.querySelectorAll('.skills__progress__item__bar-fill');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
